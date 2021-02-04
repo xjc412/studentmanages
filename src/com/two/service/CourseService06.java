cdfa8fee323f7af94096a5a0b6c2c04b14e2918c
unknown <972008448@qq.com>
2021-02-04 20:19:01 +0800

    韩松“ ; git commit -m韩松“

---
+++ src/com/two/service/CourseService06.java
@@ -0,0 +1,16 @@
+package com.two.service;
+
+
+import com.two.entity.Course;
+
+import java.util.List;
+
+public interface CourseService06 {
+    public List<Course> selectAll(int pageNo, int PageSize);
+    public int getCount();
+    public int delete(String id);
+    public int insert(String con,String cname, String cteacher, Integer ccredit);
+    public List<Course> selectByCname(String cname);
+    public int update(String cname, String cteacher, Integer ccredit, String cno);
+}
+
