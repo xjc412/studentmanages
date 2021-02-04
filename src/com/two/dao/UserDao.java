dc1867f259ab0ae7d17b3799f1ba8aa64c302d5d
shuaibisong <77102372+shuaibisong@users.noreply.github.com>
2021-02-04 17:36:30 +0800

    Delete src/com/two directory

--- src/com/two/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.two.dao;
-
-import com.two.entity.User;
-
-public interface UserDao {
-    public User loginSelect(String userName, String password);
-    public int register(String userName, String password, String level);
-}
--- src/com/two/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.two.service;
-
-import com.two.entity.User;
-
-public interface UserService {
-    public User loginSelect(String userName, String password);
-    public int register(String userName, String password, String level);
-}
